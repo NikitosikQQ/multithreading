770a62019bd8dbb2e9e33e22384761d7613475c5
NikitosikQQ <akmn00@mail.ru>
2023-07-20 20:37:18 +0300

    deadlock change 2

--- java/MyDeadlock.java
+++ java/MyDeadlock.java
@@ -1,6 +1,10 @@
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 public class MyDeadlock {
     public static final Object lock1 = new Object();
     public static final Object lock2 = new Object();
+    public static final CyclicBarrier cyclicBarrier = new CyclicBarrier(2);
 
     public static void main(String[] args) throws InterruptedException {
         ThreadOne threadOne = new ThreadOne();
@@ -9,6 +13,7 @@ public class MyDeadlock {
         threadTwo.start();
         threadOne.join();
         threadTwo.join();
+
     }
 }
 
@@ -16,14 +21,15 @@ class ThreadOne extends Thread {
     @Override
     public void run() {
         synchronized (MyDeadlock.lock1) {
-
             System.out.println("Тред1 - захвачен лок1");
+            System.out.println("Тред1 - пытается захватить лок2");
             try {
-                ThreadTwo.sleep(15);
+                MyDeadlock.cyclicBarrier.await();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
+            } catch (BrokenBarrierException e) {
+                throw new RuntimeException(e);
             }
-            System.out.println("Тред1 - пытается захватить лок2");
             synchronized (MyDeadlock.lock2) {
                 System.out.println("Тред1 - захвачен лок2 и лок1");
             }
@@ -36,13 +42,14 @@ class ThreadTwo extends Thread {
     public void run() {
         synchronized (MyDeadlock.lock2) {
             System.out.println("Тред2 - захвачен лок2");
+            System.out.println("Тред2 - пытается захватить лок1");
             try {
-                ThreadTwo.sleep(15);
+                MyDeadlock.cyclicBarrier.await();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
+            } catch (BrokenBarrierException e) {
+                throw new RuntimeException(e);
             }
-            System.out.println("Тред2 - пытается захватить лок1");
-
             synchronized (MyDeadlock.lock1) {
                 System.out.println("Тред2 - захвачен лок1 и лок2");
             }
